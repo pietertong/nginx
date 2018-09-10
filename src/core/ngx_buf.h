9c4d54031b3b9a8a28d346d07aa999bc104c24fa
Pieter Tong <339208533#qq.com>
2018-09-11 00:19:39 +0800

    type : refactor topick : Nginx内存池类型，Nginx 缓冲区数据类型，Nginx chain数据类型， detail : Nginx数据类型分为 整型 字符串 内存池 缓冲区 链表 五种基本数据类型
    Signed-off-by: Pieter Tong <339208533#qq.com>

--- src/core/ngx_buf.h
+++ src/core/ngx_buf.h
@@ -12,53 +12,98 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+/**
+ * 缓冲区结构
+ */
 typedef void *            ngx_buf_tag_t;
 
 typedef struct ngx_buf_s  ngx_buf_t;
 
+/**
+ *
+ */
 struct ngx_buf_s {
-    u_char          *pos;
-    u_char          *last;
+    u_char          *pos; /*缓冲区数据在内存的起始位置*/
+    u_char          *last;/*缓冲区数据在内存的结束位置*/
+    /**
+     * 这两个参数是处理文件时使用，类似于缓冲去的 post,last
+     */
     off_t            file_pos;
     off_t            file_last;
 
+    /**
+     * 由于实际数据可能包含在多个缓冲区中，则缓冲区的start 和 end指向，
+     * 这块内存的开始位置和结束地址
+     * 而 post 和 last是 指向本缓存区实际包含的数据的开始和结尾
+     */
     u_char          *start;         /* start of buffer */
     u_char          *end;           /* end of buffer */
     ngx_buf_tag_t    tag;
-    ngx_file_t      *file;
+    ngx_file_t      *file; /**指向buffer 对应的文件对象*/
+    /**
+     *当缓冲区的一个影子缓冲区，即当一个缓冲区复制另一个缓冲区的数据，就会发生相互指向对方的shadow指针
+     */
     ngx_buf_t       *shadow;
 
 
+    /**
+     * 为 1 时，表示该buf 所包含的内容在用户创建的内存块中，可以被 filter处理变更
+     */
     /* the buf's content could be changed */
     unsigned         temporary:1;
 
     /*
+     * 为 1 时，表示该 所包含的内容在内存中，不能被filter处理变更
      * the buf's content is in a memory cache or in a read only memory
      * and must not be changed
      */
     unsigned         memory:1;
 
+    /**
+     * 为 1 时，表示该 所包含的内容在内存中，可以通过mmap把文件映射到内存中，不能被filter处理变更
+     */
     /* the buf's content is mmap()ed and must not be changed */
     unsigned         mmap:1;
 
+    /**
+     * 可回收，即这些buf可以被释放；
+     */
     unsigned         recycled:1;
+    /**
+     * 表示buf所包含的内容在文件中
+     */
     unsigned         in_file:1;
+
+    /**
+     * 刷新缓冲区
+     */
     unsigned         flush:1;
+    /**
+     * 同步方式
+     */
     unsigned         sync:1;
+    /**
+     * 当前待处理的是最后一块缓冲区
+     */
     unsigned         last_buf:1;
+    /**
+     * 在当前的chain里面，该buf是最后一个，但不一定时 last_buf
+     */
     unsigned         last_in_chain:1;
 
     unsigned         last_shadow:1;
     unsigned         temp_file:1;
 
-    /* STUB */ int   num;
+    /* STUB */
+    int   num;
 };
 
-
+/**
+ * ngx_china_t 数据类型是与 缓冲区类型 ngx_buf_t相关的链表结构
+ */
 struct ngx_chain_s {
-    ngx_buf_t    *buf;
-    ngx_chain_t  *next;
+    ngx_buf_t    *buf; /*指向当前缓冲区*/
+    ngx_chain_t  *next;/*指向下一个chain，形成chain链表*/
 };
 
 
--- src/core/ngx_palloc.h
+++ src/core/ngx_palloc.h
@@ -46,22 +46,26 @@ struct ngx_pool_large_s {
 };
 
 
+/**
+ * 内存池数据结构模块
+ */
 typedef struct {
-    u_char               *last;
-    u_char               *end;
-    ngx_pool_t           *next;
-    ngx_uint_t            failed;
-} ngx_pool_data_t;
+    u_char               *last; /*当前内存分配的结束位置，即下一段可分配内存的起始位置*/
+    u_char               *end; /*内存池的结束位置*/
+    ngx_pool_t           *next;/*指向下一个内存池*/
+    ngx_uint_t            failed;/*记录内存池分配失败的次数*/
+} ngx_pool_data_t;/*维护内存池的数据块*/
 
 
+/*内存池的管理模块，即内存池头部结构*/
 struct ngx_pool_s {
-    ngx_pool_data_t       d;
-    size_t                max;
-    ngx_pool_t           *current;
-    ngx_chain_t          *chain;
-    ngx_pool_large_t     *large;
-    ngx_pool_cleanup_t   *cleanup;
-    ngx_log_t            *log;
+    ngx_pool_data_t       d; /*内存池的数据块*/
+    size_t                max; /*内存池数据块的最大值*/
+    ngx_pool_t           *current;/*指向当前内存池*/
+    ngx_chain_t          *chain;/*指向一个ngx_chain_t的结构*/
+    ngx_pool_large_t     *large;/*大块内存链表，即分配空间超过max的内存*/
+    ngx_pool_cleanup_t   *cleanup;/*析构函数，释放内存*/
+    ngx_log_t            *log;/*内存分配相关的日志信息*/
 };
 
 
