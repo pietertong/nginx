0a955b6c54d446749a6cc0a68cb32a30e3cfc4d6
PieterTong <339208533@qq.com>
2022-09-13 21:01:53 +0800

    Ngix 封装的链表容器 - 单向链表
    Signed-off-by: PieterTong <339208533@qq.com>

--- src/core/ngx_list.c
+++ src/core/ngx_list.c
@@ -8,44 +8,65 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
-ngx_list_t *
-ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
+/**
+ * 创建链表
+ * 创建链表时，首先分配链表表头，在对链表进行初始化，在初始化过程中分配头节点数据区内存
+ * @param pool
+ * @param n
+ * @param size
+ * @return
+ */
+ngx_list_t * ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
     ngx_list_t  *list;
-
+    /**
+     * 分配链表表头的内存
+     */
     list = ngx_palloc(pool, sizeof(ngx_list_t));
     if (list == NULL) {
         return NULL;
     }
-
+    /**
+     * 初始化链表
+     */
     if (ngx_list_init(list, pool, n, size) != NGX_OK) {
         return NULL;
     }
-
     return list;
 }
 
-
-void *
-ngx_list_push(ngx_list_t *l)
+/**
+ * 添加元素
+ * 添加元素到链表，都是从最后一个节点开始，
+ * 首先判断最后一个节点的数据区是否由内存存放新增加的元素，
+ * 若足以存储该新元素，则返回存储新元素内存的位置
+ * 若没有足够的内存存储新增加的元素，则分配一个新的节点，再把该新的节点链接到现有链表中，并返回存储新元素内存的位置
+ * 注意：
+ *  添加的元素可以是整数，也可以是一个结构
+ * @param l
+ * @return
+ * 添加一个元素
+ */
+void * ngx_list_push(ngx_list_t *l)
 {
     void             *elt;
     ngx_list_part_t  *last;
-
+    // last 节点指针指向链表的最后一个节点
     last = l->last;
 
     if (last->nelts == l->nalloc) {
-
+        /** 若最后一个节点的数据区已满*/
         /* the last part is full, allocate a new list part */
-
+        // 在原来的资源池中操作申请一个新的节点
         last = ngx_palloc(l->pool, sizeof(ngx_list_part_t));
         if (last == NULL) {
+            // 没有申请到
             return NULL;
         }
-
+       /** 分配新节点数据区内存，并使节点结构指向数据区的首地址*/
         last->elts = ngx_palloc(l->pool, l->nalloc * l->size);
         if (last->elts == NULL) {
+            // 没有申请到
             return NULL;
         }
 
@@ -56,8 +77,9 @@ ngx_list_push(ngx_list_t *l)
         l->last = last;
     }
 
+    //计算存储新元素的位置
     elt = (char *) last->elts + l->size * last->nelts;
-    last->nelts++;
-
+    last->nelts++; /**实际存放元素 + 1*/
+    /**返回新元素所在的位置*/
     return elt;
 }
--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -1,7 +1,16 @@
 
 /*
+ *
+ * ngx_list_t 是Nginx 封装的链表容器，链表容器内存分配是基于内存池进行的，
+ * 操作方便，效率高，
+ * Nginx 链表容器和普通链表类似，均有链表表头和链表节点，通过节点指针组成链表
  * Copyright (C) Igor Sysoev
  * Copyright (C) Nginx, Inc.
+ *
+ * 链表的操作只有两个
+ * 1、创建链表
+ * 2、添加元素
+ * 由于链表的内存妇女病是基于内存池的，所有内存的消耗有内存池进行，及链表中没有销毁操作
  */
 
 
@@ -12,42 +21,58 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+/**
+ * 链表结构
+ */
 typedef struct ngx_list_part_s  ngx_list_part_t;
 
+/**
+ * 链表中的节点结构
+ */
 struct ngx_list_part_s {
-    void             *elts;
-    ngx_uint_t        nelts;
-    ngx_list_part_t  *next;
+    void             *elts;/** 指向该节点数据区域的首地址*/
+    ngx_uint_t        nelts;/** 该节点数据区域实际存放的元素个数 */
+    ngx_list_part_t  *next; /** 指向链表的下一个节点*/
 };
 
-
+/**
+ * 链表表头的结构
+ */
 typedef struct {
-    ngx_list_part_t  *last;
-    ngx_list_part_t   part;
-    size_t            size;
-    ngx_uint_t        nalloc;
-    ngx_pool_t       *pool;
+    ngx_list_part_t  *last; /** 指向链表中最后一个节点*/
+    ngx_list_part_t   part; /** 链表中表头包含的第一个节点*/
+    size_t            size; /** 元素的字节大小*/
+    ngx_uint_t        nalloc;/** 链表中每个节点所能容纳元素个数*/
+    ngx_pool_t       *pool; /** 该链表节点空间的内存池对象*/
 } ngx_list_t;
 
 
 ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);
 
-static ngx_inline ngx_int_t
-ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
+/**
+ * 初始化链表
+ * @param list
+ * @param pool
+ * @param n
+ * @param size
+ * @return
+ */
+static ngx_inline ngx_int_t ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
+    /**
+     * 分配节点数据区内存，并返回改节点数据区域首地址
+     */
     list->part.elts = ngx_palloc(pool, n * size);
     if (list->part.elts == NULL) {
         return NGX_ERROR;
     }
-
+    /** 初始化节点成员*/
     list->part.nelts = 0;
     list->part.next = NULL;
     list->last = &list->part;
     list->size = size;
     list->nalloc = n;
     list->pool = pool;
-
     return NGX_OK;
 }
 
